1cdddc0999872629469f8ff1de5ce354fb26e91e
Fernando Cruz Cavina <fernando.cruz.cavina@gmail.com>
2026-04-09 11:31:58 -0300

    chore: initial folder structure
    contains also html structure for the main page in src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <aside></aside>
+      <main>
+      </main>
+      <aside></aside>
+      <footer></footer>
+    </>
+  )
+}
+
+export default App
